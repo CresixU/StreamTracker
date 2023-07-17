6efa695390f06ce7008eaf34525dd4e190dab778
Krystian Wawruch <CresixU@gmail.com>
2023-07-17 17:33:26 +0200

    Project Init
    Install Vue, Vue Router, SCSS, Tailwind

---
+++ streamtracker/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="container mx-auto px-4">
+	<nav class="flex items-center space-x-2 text-base">
+		<div>
+			<RouterLink to="/">Home</RouterLink>
+		</div>
+		<div>
+			<RouterLink to="streams">Streamy</RouterLink>
+		</div>
+	</nav>
+	<main>
+		<RouterView>
+			<!--<Transition name="animation">
+				<component :is="Component" />
+			</Transition>-->
+		</RouterView>
+	</main>
+</div>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<style scoped>
+.animation-enter-active,
+.animation-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease;
+}
+
+.animation-enter-from,
+.animation-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+</style>
