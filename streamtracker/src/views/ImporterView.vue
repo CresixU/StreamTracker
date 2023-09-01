861da192d2fa7c910ccc4e481e9a8af92a027390
Krystian Wawruch <CresixU@gmail.com>
2023-09-01 22:26:10 +0200

    Create ImporterView.vue

---
+++ streamtracker/src/views/ImporterView.vue
@@ -0,0 +1,120 @@
+<template>
+<div>
+    <div v-if="platforms">
+        Start task
+        <input type="file" v-on:change="handleFileUpload( $event )">
+        <VSelect :items="platforms"></VSelect>
+        <VButton @click="startTask()">Start</VButton>
+    </div>
+    <hr class="my-10">
+    <div>
+        <table class="w-full mt-6">
+            <thead>
+                <th>ID</th>
+                <th>Status</th>
+                <th>Platforma</th>
+            </thead>
+            <TransitionGroup name="animation" tag="tbody" v-if="tasks">
+                <tr v-for="task in tasks" :key="task.id">
+                    <td>
+                        {{ task.id }}
+                    </td>
+                    <td>
+                        {{ task.status }}
+                    </td>
+                    <td>
+                        {{ task.platform }}
+                    </td>
+                </tr>
+            </TransitionGroup>
+        </table>
+    </div>
+</div>
+</template>
+<script>
+import VSelect from '@/components/VSelect.vue'
+import VButton from '@/components/VButton.vue'
+
+export default {
+    components: {
+        VSelect, VButton
+    },
+    data() {
+        return {
+            statuses: null,
+            tasks: null,
+            platforms: null,
+            taskFile: null,
+            taskPlatform: '',
+            form: {
+                file: null,
+                platform: ''
+            }
+        }
+    },
+    methods: {
+        async getStatuses() {
+            const url = `${import.meta.env.VITE_API_KEY}/api/v1/streamers/import/statuses/`
+            const response = await fetch(url, {
+				headers: {
+					'Content-Type': 'application/json'
+				}
+            })
+            this.statuses = await response.json()
+        },
+        async getTasks() {
+            const url = `${import.meta.env.VITE_API_KEY}/api/v1/streamers/import/tasks/`
+            const response = await fetch(url, {
+				headers: {
+					'Content-Type': 'application/json'
+				}
+            })
+            this.tasks = await response.json()
+        },
+        async getTaskById(id) {
+            const url = `${import.meta.env.VITE_API_KEY}/api/v1/streamers/import/${id}/status/`
+            const response = await fetch(url, {
+				headers: {
+					'Content-Type': 'application/json'
+				}
+            })
+            this.tasks = await response.json()
+        },
+        async startTask() {
+            let formData = new FormData();
+
+            formData.append('platform', this.form.platform);
+            formData.append('file', this.form.file)
+
+            const url = `${import.meta.env.VITE_API_KEY}/api/v1/streamers/import/`
+            const response = await fetch(url, {
+                method: 'POST',
+				headers: {
+					'Content-Type': 'multipart/form-data'
+				},
+                body: formData
+            })
+        },
+        async getAvailablePlatforms() {
+            const url = `${import.meta.env.VITE_API_KEY}/api/v1/streamers/platforms/`
+            const response = await fetch(url, {
+                //credentials: 'include',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+            });
+			this.platforms = await response.json();
+        },
+        handleFileUpload( e ) {
+            this.form.file = e.target.files[0];
+        }
+    },
+    async created() {
+        await this.getAvailablePlatforms();
+        await this.getStatuses();
+        await this.getTasks();
+    }
+}
+
+
+</script>
