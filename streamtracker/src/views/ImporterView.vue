7acc0bd2b1798cf656b9b685635847f4d28f4576
Krystian Wawruch <CresixU@gmail.com>
2023-09-16 11:23:23 +0200

    Add modal info

--- streamtracker/src/views/ImporterView.vue
+++ streamtracker/src/views/ImporterView.vue
@@ -1,14 +1,25 @@
 <template>
 <div>
+    <VModalChoice
+		v-show="(requestLoading && modalVisible) || modalVisible">
+		<template v-slot:header>
+			Informacja
+		</template>
+		<template v-slot:body class=" text-center">
+			<p v-if="requestLoading">Trwa dodawanie nowego taska <br> Nie powinno to zająć dłużej, niż 10 sekund <br> Nie opuszczaj strony</p>
+            <p v-else>Task zakończył swoje działanie, można opuścić stronę</p>
+		</template>
+		<template v-slot:footer class="flex justify-evenly">
+			<VButton v-show="!requestLoading" @click="modalVisible = false">OK</VButton>
+		</template>
+	</VModalChoice>
     <div v-if="platforms">
         <h2 class=" text-lg font-bold mb-3">Start task</h2>
-        <form id="form">
             <div class="flex">
                 <input type="file" id="file" v-on:change="handleFileUpload( $event )">
                 <VSelect :items="platforms" style="min-width: 200px;" class="mx-5" id="platform" v-model="form.platform"></VSelect>
                 <VButton @click="startTask()">Start</VButton>
             </div>
-        </form>
     </div>
     <div class="my-10"></div>
     <div>
@@ -38,10 +49,12 @@
 <script>
 import VSelect from '@/components/VSelect.vue'
 import VButton from '@/components/VButton.vue'
+import VModalChoice from '@/components/VModalChoice.vue'
+import axios from  'axios'
 
 export default {
     components: {
-        VSelect, VButton
+        VSelect, VButton, VModalChoice
     },
     data() {
         return {
@@ -53,7 +66,9 @@ export default {
             form: {
                 file: null,
                 platform: ''
-            }
+            },
+            requestLoading: false,
+            modalVisible: false
         }
     },
     methods: {
@@ -85,27 +100,32 @@ export default {
             this.tasks = await response.json()
         },
         async startTask() {
-            let formData = new FormData(form);
-
-            formData.append('platform', this.form.platform);
-            formData.append('file', this.form.file)
-            try {
-                const url = `${import.meta.env.VITE_API_KEY}/api/v1/streamers/import/`
-                const response = await fetch(url, {
-                    method: 'POST',
-				    headers: {
-					    'Content-Type': 'multipart/form-data;  boundary=----WebKitFormBoundaryyEmKNDsBKjB7QEqu'
-				    },
-                    body: formData
+            this.requestLoading = true;
+            this.modalVisible = true;
+            console.log("start modal "+this.requestLoading)
+            const url = `${import.meta.env.VITE_API_KEY}/api/v1/streamers/import/`
+            const formData = new FormData();
+                formData.append('file', this.form.file);
+                formData.append('platform', this.form.platform);
+                const headers = { 'Content-Type': 'multipart/form-data' };
+                 
+                await axios.post(url, formData, { headers })
+                .then(async (res) => {
+                    res.data.files; // binary representation of the file
+                    res.status; // HTTP status
+                    this.form.file = null;
+                    this.form.platform = '';
+                    await this.getTasks();
+                    console.log("off modal "+this.requestLoading)
+                    this.requestLoading = false;
                 })
+                .catch(
+                    (err) => {
+                        console.log("off modal "+this.requestLoading)
+                        this.requestLoading = false;
+                    }
+                );
                 
-                const result = await response.json();
-                await this.getTasks();
-            }
-            catch (error) {
-                alert('Nie udało sie wystartować taska');
-                console.log(error)
-            }
         },
         async getAvailablePlatforms() {
             const url = `${import.meta.env.VITE_API_KEY}/api/v1/streamers/platforms/`
